d33f2d9725779b06185ed72d93b3c87538de1054
danielevalverde <danierzevalverde@gmail.com>
2023-11-09 20:53:25 -0300

    add DPS printing output in terminal

--- dps_terminal.py
+++ dps_terminal.py
@@ -1,4 +1,8 @@
-import matplotlib.pyplot as plt
+from colorama import init, Fore, Back
+
+# Inicializa o colorama
+init()
+
 # Função de busca em profundidade
 def busca_gato_labirinto(labirinto, linha, coluna, visitados, caminho_atual):
     if linha < 0 or coluna < 0 or linha >= len(labirinto) or coluna >= len(labirinto[0]) or labirinto[linha][coluna] == 0 or (linha, coluna) in visitados:
@@ -7,26 +11,24 @@ def busca_gato_labirinto(labirinto, linha, coluna, visitados, caminho_atual):
     caminho_atual.append((linha, coluna))
     print(f'Explorando posição: ({linha}, {coluna})')
     print('Caminho percorrido até agora:', caminho_atual)
+    desenhar_labirinto_terminal(labirinto, caminho_atual)  # Adicionando a função de desenho aqui
     busca_gato_labirinto(labirinto, linha+1, coluna, visitados, caminho_atual)
     busca_gato_labirinto(labirinto, linha-1, coluna, visitados, caminho_atual)
     busca_gato_labirinto(labirinto, linha, coluna+1, visitados, caminho_atual)
     busca_gato_labirinto(labirinto, linha, coluna-1, visitados, caminho_atual)
     caminho_atual.pop()
 
-# Função para desenhar o labirinto
-def desenhar_labirinto(labirinto, caminho):
-    fig, ax = plt.subplots()
+# Função para desenhar o labirinto no terminal
+def desenhar_labirinto_terminal(labirinto, caminho):
     for i in range(len(labirinto)):
         for j in range(len(labirinto[0])):
-            if labirinto[i][j] == 1:
-                ax.add_patch(plt.Rectangle((j, -i-1), 1, 1, color='white'))
+            if (i, j) in caminho:
+                print(Fore.GREEN + 'X', end=' ')
+            elif labirinto[i][j] == 1:
+                print(Back.WHITE + ' ', end=' ')
             else:
-                ax.add_patch(plt.Rectangle((j, -i-1), 1, 1, color='black'))
-    for pos in caminho:
-        ax.add_patch(plt.Rectangle((pos[1], -pos[0]-1), 1, 1, color='lightgreen'))
-    ax.set_aspect('equal', adjustable='box')
-    plt.axis('off')
-    plt.show()
+                print(Back.BLACK + ' ', end=' ')
+        print(Back.RESET)
 
 # Definindo o labirinto como uma matriz
 labirinto = [
@@ -42,7 +44,3 @@ visitados = set()
 caminho_atual = []
 print("Começando a busca pelo gato no labirinto...")
 busca_gato_labirinto(labirinto, 0, 0, visitados, caminho_atual)
-
-# Desenhando o labirinto com o caminho percorrido
-# plt.draw()
-desenhar_labirinto(labirinto, caminho_atual)
