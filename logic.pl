54a9566f6973757f76d1034a6d14e6fe6168082c
danielevalverde <danierzevalverde@gmail.com>
2023-11-23 19:42:28 -0300

    add more complexity

--- logic.pl
+++ logic.pl
@@ -1,13 +1,19 @@
 start :-
     write('Bem-vindo ao mistério da Mansão Ravenwood!'), nl,
-    write('Um assassinato chocante aconteceu na casa do famoso magnata, Sr. Black.'), nl,
-    write('Seu corpo foi descoberto na sala principal da mansão.'), nl,
-    write('A polícia identificou seis suspeitos: o mordomo, a senhora da casa, o professor da filha do Sr. Black, o coveiro local, a governanta e o jardineiro.'), nl,
-    write('Além disso, cinco armas foram encontradas perto do local do crime: uma faca, uma corda, um revólver, um candelabro e um veneno.'), nl,
-    write('Os locais da casa incluem a sala principal, a cozinha, o jardim, o sótão e o escritório.'), nl,
+    write('Na noite do assassinato, um baile extravagante estava em pleno andamento na suntuosa mansão.'), nl,
+    write('Entre os eventos da noite, os convidados desfrutavam de um cardápio refinado com iguarias como filé mignon, camarões grelhados e sobremesas luxuosas.'), nl,
+    write('O mordomo, conhecido por sua atenção aos detalhes, supervisionava pessoalmente o serviço de bebidas.'), nl,
+    write('A senhora da casa, uma entusiasta de música clássica, encantava os convidados tocando piano na sala principal.'), nl,
+    write('O professor da filha do Sr. Black, um aficionado por astronomia, foi visto observando as estrelas no jardim.'), nl,
+    write('O coveiro local, que adora jardinagem, estava examinando as flores do jardim de inverno.'), nl,
+    write('A governanta, responsável pela organização do evento, circulava entre os convidados, atendendo às suas necessidades.'), nl,
+    write('O jardineiro, frequentemente visto cuidando do jardim, estava especialmente ocupado com a manutenção das plantas durante o baile.'), nl,
+    write('Apesar de todo o glamour, os detalhes sobre as atividades de cada suspeito durante o baile são um tanto nebulosos.'), nl,
+    write('Várias testemunhas relataram que o culpado estava usando uma camisa vermelha na noite do crime.'), nl,
     write('Digite "suspeito_no_local(X, Local)." para saber se um suspeito esteve em um local específico.'), nl,
     write('Digite "suspeitos_no_local(Local)." para saber quem estava no local do crime.'), nl,
     write('Digite "culpado(X)." para verificar quem é o culpado.'), nl.
+	write('Digite "cor_da_camisa_do_suspeito(Suspeito, Cor)." para descobrir a cor da camisa de um suspeito.'), nl.
 
 
 % Suspeitos
@@ -48,6 +54,16 @@ usou(mordomo, corda).
 usou(senhora, revolver).
 usou(coveiro, candelabro).
 
+/* Defina regras sobre a cor da camisa de cada suspeito */
+cor_da_camisa(professor, vermelha).
+cor_da_camisa(mordomo, azul).
+cor_da_camisa(senhora, verde).
+cor_da_camisa(coveiro, amarela).
+cor_da_camisa(filha_professor, branca).
+cor_da_camisa(guarda_florestal, preto).
+cor_da_camisa(cozinheiro, vermelha).
+cor_da_camisa(advogado, azul).
+
 /* Defina regras sobre onde cada suspeito estava */
 onde_estava(mordomo, sala_principal).
 onde_estava(senhora, cozinha).
@@ -73,4 +89,4 @@ suspeitos_no_local(_).
 % Regras para determinar o culpado
 /* Atualize a regra para determinar o culpado */
 % Regras para determinar o culpado
-culpado(X) :- suspeito(X), esteve(X, estudo), usou(X, faca).
+culpado(X) :- suspeito(X), esteve(X, estudo), usou(X, faca), usava_camisa_vermelha(X).
